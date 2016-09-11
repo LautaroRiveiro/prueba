cd87763aeefa295d536bd888ff24249ff6c42b12
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2016-09-11 00:27:00 -0300

    vbxcvb

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,24 +1,61 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function($scope, $cordovaNativeAudio, $timeout, $cordovaVibration, $ionicPlatform) {
   $scope.Saludar = function(){
     alert("HOLA!");
+    console.log("PROBANDO");
   }
+
+  $scope.Vibrar = function(){
+    $cordovaVibration.vibrate(1000);
+  };
+
+/*var audio = new Audio('audio/1.mp3');
+ $scope.Sonido = function(){
+audio.play();
+}*/
+  $ionicPlatform.ready(function() {
+    if( window.plugins && window.plugins.NativeAudio ) {
+      window.plugins.NativeAudio.preloadSimple( 'click', 'audio/click.mp3', function(msg){
+      }, function(msg){
+          console.log( 'error: ' + msg );
+      });
+    }
+  });
+
+  $scope.Sonido = function(){
+    window.plugins.NativeAudio.play( 'click' );
+  }
+
 })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('ChatsCtrl', function($scope, $cordovaNativeAudio, $timeout, $cordovaVibration) {
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
+  $scope.Vibrar = function(){
+    $cordovaVibration.vibrate(1000);
   };
+
+/*var audio = new Audio('audio/1.mp3');
+ $scope.Sonido = function(){
+audio.play();
+}*/
+
+if( window.plugins && $cordovaNativeAudio){
+  //Funciones del plugin Sonidos
+  $cordovaNativeAudio.preloadSimple('click', 'audio/1.mp3');
+
+  $scope.Sonido = function(){
+    console.log("PROBANDO");
+    $cordovaNativeAudio.play('click');
+    console.log("PROBANDO");
+    //$cordovaNativeAudio.loop('music');
+  }
+}
+else{
+  alert("ERROR");
+}
+
+
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
